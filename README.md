6b859029977a2f216d37ec64ff7a9cb0cf3e73b1
miguel-merlin <mmerlin@stevens.edu>
2023-04-13 12:53:35 -0400

    Dot product and euclidean norm

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Quantumnet
-## Python library for quantum computing
+### Python library for quantum computing
+Python library to do quantum computing calculations. It includes vectors operations, vector spaces, Hilbert spaces, matrices, transformations. 
--- quantumnet/vector.py
+++ quantumnet/vector.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Vector:
     def __init__(self, vectorComponents):
         if (not vectorComponents):
@@ -6,6 +8,12 @@ class Vector:
             raise ValueError("The list of vector components can only contain numbers")
         self._vectorComponents = vectorComponents
         self._vectorSpace = len(vectorComponents)
+    
+    def getVectorComponents(self):
+        return self._vectorComponents
+
+    def getVectorSpace(self):
+        return self._vectorSpace
     
     def __str__(self) -> str:
         vector = "<"
@@ -18,13 +26,29 @@ class Vector:
         return vector
     
     def __eq__(self, otherVector: object) -> bool:
-        if len(self._vectorComponents) != len(otherVector._vectorComponents):
+        if len(self._vectorComponents) != len(otherVector.getVectorSpace()):
             return False
         else:
             for i in range(0, self._vectorSpace):
-                if self._vectorComponents[i] != otherVector._vectorComponents[i]:
+                if self._vectorComponents[i] != otherVector.getVectorSpace()[i]:
                     return False
                 else: continue
         return True
-
+    
+    def dot_product(self, otherVector: object) -> float:
+        if self._vectorSpace != otherVector.getVectorSpace():
+            raise ValueError("Cannot compute dot product because the vectors have different vector space")
+        dot_product = 0
+        for i in range(0, self.getVectorSpace()):
+            dot_product += (self.getVectorComponents[i] * otherVector.getVectorComponents()[i])
+        return dot_product
+    
+    def euclidean_norm(self) -> float:
+        if not self.getVectorComponents:
+            raise ValueError("The vector is empty <>")
+        sum_of_squares = 0
+        for i in range(self._vectorSpace):
+            sum_of_squares += pow(self._vectorComponents[i], 2)
+        
+        return sqrt(sum_of_squares)
     
